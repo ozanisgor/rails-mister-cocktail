d0c661c8d81645ec8eebb7de4d0e8e88d52891ff
ozan isgor <ozanisgor11@gmail.com>
2020-10-16 15:23:06 +0300

    small fix

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
 class Ingredient < ApplicationRecord
   has_many :doses
   validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
